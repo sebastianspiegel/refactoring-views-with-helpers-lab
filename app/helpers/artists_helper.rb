a3e70d9c3f2d4c654b53accdfc64492606672ac0
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-01-10 11:12:02 -0500

    commit tree 305998904e6260def05b8565c269b8152f70f5d7

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,13 @@
 module ArtistsHelper
+
+    def display_artist(song)
+        if song.artist && song.artist.name != ""
+            link_to song.artist.name, artist_path(song.artist)
+        else
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
 end
+
+# If an artist is already associated with the song, return a link to the artist's show page
+# If an artist is not associated with the song (a.k.a. 'else'), return a link to the song's edit page, with a link text of "Add Artist"
